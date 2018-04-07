1acbc426aebdf84778463f48383cfcdc496337ab
git-sergBond <git-serg.bond@yandex.ru>
2018-04-08 00:55:54 +0300

    добился, что работает вызов функции из библиотеки

--- xxx/projectDLLtest/unit1.pas
+++ xxx/projectDLLtest/unit1.pas
@@ -1,55 +1,55 @@
 unit Unit1;
 
-{$mode objfpc}{$H+}
+{$mode delphi}{$H+}
 
 interface
 
 uses
- Windows{ !!! }, Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
- StdCtrls;
+Windows{ !!! }, Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
+StdCtrls;
 {-------}
 var result: Integer;
-     LibHandle: Hmodule;
-     Name1: Pointer;//function(): Integer; stdcall;
+LibHandle: Hmodule;
+Name1: function(): Integer; stdcall;
 {-------}
+
 type
 
-  { TForm1 }
+{ TForm1 }
 
-  TForm1 = class(TForm)
-    Button1: TButton;
-    procedure Button1Click(Sender: TObject);
-  private
+TForm1 = class(TForm)
+Button1: TButton;
+procedure Button1Click(Sender: TObject);
+private
 
-  public
+public
 
-  end;
+end;
 
 var
-  Form1: TForm1;
+Form1: TForm1;
 
 implementation
 
 {$R *.lfm}
 
 { TForm1 }
-
 procedure TForm1.Button1Click(Sender: TObject);
 
 begin
-  LibHandle := LoadLibrary('CR95HF.dll'); // загружаем DLL
-  if LibHandle <> 0 then
-  begin
-      ShowMessage('библиотека подгружена');
-      Name1 := nil;
-      Name1 := GetProcAddress(LibHandle, 'Get_Dll_rev'); // запоминаем адрес необходимой функции
-       if Name1 <> nil then
-       begin
-         ShowMessage('функция найдена');
-       end;
-  end;
-  FreeLibrary(LibHandle); // выгружаем DL
+LibHandle := LoadLibrary('CR95HF.dll'); // загружаем DLL
+if LibHandle <> 0 then
+begin
+ShowMessage('библиотека подгружена');
+Name1 := nil;
+@Name1 := GetProcAddress(LibHandle, 'CR95HFDLL_USBhandlecheck'); // запоминаем адрес необходимой функции
+if @Name1 <> nil then
+begin
+ShowMessage('функция найдена');
+ShowMessage(inttostr(Name1()));
+end;
+end;
+FreeLibrary(LibHandle); // выгружаем DL
 end;
 
 end.
-
